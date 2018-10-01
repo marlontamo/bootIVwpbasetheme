1b51fa75a94baeb71a628d760a31ad77e189b0fe
marlontamo <marlontamo@gmail.com>
2018-10-01 10:15:44 +0800

    fixed tether.io error
    Signed-off-by: marlontamo <marlontamo@gmail.com>

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 </div>
+<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
 <?php wp_footer(); ?>
 <?php get_template_part( '/template/content', 'footer' ); ?>
 <script type="text/javascript">
